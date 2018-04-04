27844d007e68010c454e0ba4f53a9539381f71a5
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:45:55 -0700

    PNX_sprout: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Ia6a6f94696f2605dc2f3a932ea598f3cdeb3bed0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2018-2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_PNX_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_PNX_sprout-user \
+    arrow_PNX_sprout-userdebug \
+    arrow_PNX_sprout-eng
