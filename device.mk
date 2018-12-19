81ea8446ec05410b247e6781c24ffe780b78f6e0
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 21:57:31 +0530

    phoenix: Build telephony-ext
    Change-Id: I2dd7d94ba00af4d20aa2410b7c5ee54726a6ed59

--- device.mk
+++ device.mk
@@ -371,6 +371,10 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Update engine
 PRODUCT_PACKAGES += \
