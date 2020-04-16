29e1ad6b717ee0f15cd378814972ab4d347a511c
Rick Chen <rickctchen@google.com>
2020-04-16 09:32:00 +0200

    phoenix: sensors: Enable multihal 2.0
    * Phoenix requires android.hardware.sensors@2.0-service.multihal
    
    Change-Id: I582b0b224bc507aff426c338f6b7067465fb5c97

--- device.mk
+++ device.mk
@@ -362,6 +362,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal 
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
 
