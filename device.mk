81268d7a7a571d8d9d5caf30ced9e42343f320dd
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-31 15:46:14 +0530

    phoenix: Explictly build vendor.qti.hardware.camera.device@1.0
    * 64-bit variant isn't being built by default
    
    Change-Id: I75854d887464de063d1bf031c065cc1cc6c6c9b8

--- device.mk
+++ device.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.6
+    android.hardware.camera.provider@2.6 \
+    vendor.qti.hardware.camera.device@1.0
 
 # HIDL
 PRODUCT_PACKAGES += \
