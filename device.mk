9e4b22e7b45f5986a3082df9faa0b6783cf26b47
Ethan Chen <intervigil@gmail.com>
2020-10-22 14:38:37 +0530

    phoenix: Convert libhidl to blueprint
    Change-Id: Ief105a44eeb403b2fa41d391a0d72c776da13248

--- device.mk
+++ device.mk
@@ -39,9 +39,11 @@ PRODUCT_PACKAGES_DEBUG += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
+    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system \
+    android.hidl.manager@1.0.vendor
+
+PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
