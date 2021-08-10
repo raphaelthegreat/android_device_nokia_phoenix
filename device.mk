eddfb56058bdc733672c52ff971bb77aca05ae75
George <geoster3d@gmail.com>
2021-08-10 12:35:02 +0300

    phoenix: Build clearkey HAL

--- device.mk
+++ device.mk
@@ -116,7 +116,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.3-service.clearkey
 
 # HIDL
 PRODUCT_PACKAGES += \
