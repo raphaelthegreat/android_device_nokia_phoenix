881c2ee62be30f77cd3689245bf40fdd54080d21
George <geoster3d@gmail.com>
2021-08-10 13:35:04 +0300

    phoenix: Cleanup wifi stack

--- device.mk
+++ device.mk
@@ -352,7 +352,9 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
