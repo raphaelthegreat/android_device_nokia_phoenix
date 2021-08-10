699e4a576148abd52d4a9f9b7ccc9af96d90a955
George <geoster3d@gmail.com>
2021-08-10 13:27:29 +0300

    phoenix: Import and build samsung NFC HAL

--- device.mk
+++ device.mk
@@ -228,6 +228,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service.samsung \
+    NfcNci \
+    Tag \
+    com.android.nfc_extras
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
