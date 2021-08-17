5a8dc265905729bd995e9c30110e30928d652550
George <geoster3d@gmail.com>
2021-08-17 12:25:41 +0300

    phoenix: Build USB service

--- device.mk
+++ device.mk
@@ -435,6 +435,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
