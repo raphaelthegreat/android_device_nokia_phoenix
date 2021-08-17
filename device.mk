9491c198d82a7b998e96ea1b014014ddcffed919
George <geoster3d@gmail.com>
2021-08-17 13:05:25 +0300

    phoenix: Build renderscript HAL

--- device.mk
+++ device.mk
@@ -398,6 +398,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ueventd.qcom.rc \
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
