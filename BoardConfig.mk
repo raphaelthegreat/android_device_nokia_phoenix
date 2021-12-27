f6c3079b826d6fca378be63b1993b17674fa5724
kaloresanket <kaloresanket@gmail.com>
2021-12-27 16:12:02 +0530

    PNX_sprout: Enable DT2W

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/AllHWList/tp_double_tap"
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
