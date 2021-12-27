0b0bdce22b5441a080ee9f72969a23cd5ff5ce48
Danny Baumann <dannybaumann@web.de>
2021-12-27 15:44:32 +0530

    PNX_sprout: Enable call recording

--- device.mk
+++ device.mk
@@ -368,7 +368,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 # Permissions
 PRODUCT_COPY_FILES += \
