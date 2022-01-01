25aec7b9504ea33a5629035229e876285f278917
emufan4568 <geoster3d@gmail.com>
2022-01-01 18:09:59 +0200

    PNX_sprout: Drop unused init.qcom.sensors.sh
    * The directory /vendor/etc/sensors/scripts doesn't exist anymore and it just generates denials anyway
    
    avc: denied { open } for path="/data/vendor/sensors/scripts" dev="mmcblk0p89" ino=6217793 scontext=u:r:qti_init_shell:s0 tcontext=u:object_r:sensors_vendor_data_file:s0 tclass=dir permissive=1

--- device.mk
+++ device.mk
@@ -477,7 +477,6 @@ PRODUCT_PACKAGES += \
     init.mdm.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.sensor.e2pwrite.sh \
